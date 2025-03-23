41922ddc29372b7e0fb3989d30273e37b3cef246
Andrey Sergeev <SergeevAndreyAleksandrovich@yandex.ru>
2025-03-23 13:27:05 +0300

    made exercise 2 and 3

---
+++ Exercise_3.3.js
@@ -0,0 +1,19 @@
+function count(a) {
+  if (1 < a <= 1000) {
+  let arr = new Array(a); // создаем массив указанной длины
+  for (let e; e <= arr.length - 1; e++) { // заполняем массив
+    arr.push(e)
+  }
+    for (let i; i < arr.length - 1; i++) {
+      if (a % arr[i] === 0) { // если число делится без остатка, только на себя,
+        console.log("Число непростое");  // то число простое
+      } else {
+        console.log("Число простое");
+      }
+    }
+} else {
+  console.log("Введите другое число");
+  }
+}
+
+count(10)
